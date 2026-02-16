380cde413d4a2943dcf12102cf3c69ace5918aa7
Daniel Cook <61596750+dc0820@users.noreply.github.com>
2026-02-15 23:12:11 -0600

    Change iframe source URL from vercel to danielcook.org

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'https://2-dos.vercel.app/';
+        iframe.src = 'https://danielcook.org/';
         /**
          * Use dev server is query params are present
          *
